57abd8d79ba26704b22dfb3c47024e3f0fba4abd
Pritam <riju.coder@gmail.com>
2025-05-29 10:31:34 +0530

    working with multiple files

---
+++ Basics/20_multiple_file__compilation/compare.cpp
@@ -0,0 +1,16 @@
+
+
+int min( int a, int b){
+    if(a<b)
+        return a;
+    else
+        return b;
+}
+
+
+int max(int a, int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}
