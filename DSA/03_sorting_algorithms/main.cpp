8c68fa4de5e85160575628ac656b15c3efdfb382
Pritam <riju.coder@gmail.com>
2025-06-08 16:25:49 +0530

    slection & bubble

---
+++ DSA/03_sorting_algorithms/main.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    int* selection_sort(int arr[],int n){
+        for (int i = 0; i < n-1; i++)
+        {
+            int min=i;
+            for (int j = i+1; j < n; j++)
+            {
+                if (arr[min]>arr[j])
+                {
+                    min=j;
+                }
+            }
+            swap(arr[min],arr[i]);
+
+        }
+        return arr;
+    }
+    int* bubble_sort(int arr[],int n){
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = 0; j < n-i-1; j++)
+            {
+                if (arr[j+1]<arr[j])
+                {
+                    swap(arr[j+1],arr[j]);
+                }
+            }
+        }
+        return arr;
+    }
+
+};
+
+int main(){
+    int arr[6]={282,93,938,84,22,232};
+    Solution sol;
+    sol.bubble_sort(arr,6);
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
