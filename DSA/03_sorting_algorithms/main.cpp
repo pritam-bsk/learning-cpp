58eccec5dfbc17db787f024e531c3dd75e0a7ca3
Pritam <riju.coder@gmail.com>
2025-06-08 16:35:04 +0530

    sorting

--- DSA/03_sorting_algorithms/main.cpp
+++ DSA/03_sorting_algorithms/main.cpp
@@ -33,16 +33,26 @@ public:
         }
         return arr;
     }
+    int* insertion_sort(int arr[],int n){
+        
+    }
 
 };
 
 int main(){
     int arr[6]={282,93,938,84,22,232};
     Solution sol;
-    sol.bubble_sort(arr,6);
+    sol.selection_sort(arr,6);
     for (int i = 0; i < 6; i++)
     {
         cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int arr1[]={282,93,938,84,22,232};
+    sol.bubble_sort(arr1,6);
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr1[i]<<" ";
     }
     cout<<endl;
 }
