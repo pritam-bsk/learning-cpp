70bf7a1c9e0cf778c4680ed1cb2ba79af24417bd
Pritam <riju.coder@gmail.com>
2025-06-14 21:47:26 +0530

    must do twice

---
+++ leetcode_solution/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n==1) return 0;
+        int min = prices[0]
+        for(int i{};i<n;i++){
+            min = min(min,prices[i]);
+        }
+
+    }
+};
