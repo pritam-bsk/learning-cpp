1e5f78721f8cdc473fa4670a91f31b75d75a64b1
Pritam <riju.coder@gmail.com>
2025-06-18 19:28:04 +0530

    prefix sum algorithm

---
+++ leetcode_solution/560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        int sum = 0;
+        unordered_map<int, int> hash;
+        hash[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int rem = sum - k;
+            count += hash[rem];
+            hash[sum] += 1;      
+        }
+        return count;
+    }
+};
